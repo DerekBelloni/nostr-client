7049ab14dfa3384eadcd2ce405d58b2978e6c111
Derek Belloni <derekbelloni10@gmail.com>
2024-05-26 12:04:56 -0600

    brought in a logo, abstracted the sidebar to its own component, implemented icons for all the sidebar menu items

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,30 +1,6 @@
 <template>
     <div class="flex h-screen">
-        <div class="sidebar border border-r border-gray-200 shadow">
-            <div class="flex flex-col space-y-8">
-                <div class="mt-12 flex ml-16">
-                    <h3 class="text-rose-500 font-semibold text-2xl">Logo</h3>
-                </div>
-                <div>
-                    <ul class="ml-16 space-y-8">
-                        <li>Menu</li>
-                        <li>Explore</li>
-                        <li>Messages</li>
-                        <li>Relays</li>
-                        <li>Bookmarks</li>
-                        <li>Notifications</li>
-                        <li>Downloads</li>
-                        <li>Settings</li>
-                    </ul>
-                </div>
-                <div class="ml-16">
-                    <div>
-                        <span class="text-xs ml-2">Welcome to nostr!</span>
-                    </div>
-                    <button class="rounded-full bg-rose-500 px-4 py-1 text-white font-semibold">Get Started</button>
-                </div>
-            </div>
-        </div>
+        <Sidebar class="sidebar border border-r border-gray-200 shadow"></Sidebar>
         <Test></Test>
     </div>
 </template>
@@ -32,6 +8,9 @@
 <script setup>
 import { Head, Link } from '@inertiajs/vue3';
 import Test from '../Components/Test.vue'
+import Sidebar from '../Components/Sidebar.vue'
+
+
 </script>
 
 <style scoped>
--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,12 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import PrimeVue from 'primevue/config';
+import Button from 'primevue/button';
+
+// import 'primevue/resources/themes/lara-dark-amber/theme.css'
+import 'primevue/resources/themes/aura-light-amber/theme.css';
+import 'primeicons/primeicons.css';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -14,7 +20,10 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
+            // .use(PrimeVue, { unstyled: true })
+            .use(PrimeVue)
             .use(ZiggyVue)
+            .component('Button', Button)
             .mount(el);
     },
     progress: {
