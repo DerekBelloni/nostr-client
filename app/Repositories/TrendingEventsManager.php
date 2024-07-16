cb41320d1ac950aaa5400670ac928823d18e7b39
Derek Belloni <derekbelloni10@gmail.com>
2024-07-15 20:40:39 -0600

    continued building api for trending contents, retrieving and processing videos and images

--- app/Repositories/TrendingEventsManager.php
+++ app/Repositories/TrendingEventsManager.php
@@ -9,14 +9,17 @@ class TrendingEventsManager
 {
     public static function index(Request $request)
     {
-        $trending_notes = self::_getTrendingNotes();
+        $client = new Client();
+
+        $trending_notes = self::_getTrendingNotes($client);
+        $trending_videos = self::_getTrendingVideos($client);
+        $trending_images = self::_getTrendingImages($client);
+        
         return $trending_notes;
     }
 
-    private static function _getTrendingNotes()
+    private static function _getTrendingNotes($client)
     {
-        $client = new Client();
-
         $response = $client->request('GET', 'https://api.nostr.band/v0/trending/notes', [
             'headers' => [
                 'Accept' => 'application/json'
@@ -28,18 +31,53 @@ class TrendingEventsManager
         $trending_notes = json_decode($body->getContents(), true);
         $trending_notes = collect($trending_notes["notes"]);
 
-        $processed_notes = self::_processNotes($trending_notes);
-
+        $processed_notes = self::_processContent($trending_notes);
         return $processed_notes;
     }
 
-    private static function _processNotes(&$trending_notes)
+    private static function _getTrendingVideos($client)
     {
-        return $trending_notes->transform(function ($note) {
+        $response = $client->request('GET', 'https://api.nostr.band/v0/trending/videos', [
+            'header' => [
+                'Accept' => 'application/json'
+            ]
+        ]);
+
+        $body = $response->getBody();
+
+        $trending_videos = json_decode($body->getContents(), true);
+        $trending_videos = collect($trending_videos["videos"]);
+
+        $processed_videos = self::_processContent($trending_videos);
+        return $trending_videos;
+    }
+
+    private static function _getTrendingImages($client) 
+    {
+        $response = $client->request('GET', 'https://api.nostr.band/v0/trending/images', [
+            'header' => [
+                'Accept' => 'applicatopm/json'
+            ]
+        ]);
+
+        $body = $response->getBody();
+
+        $trending_images = json_decode($body->getContents(), true);
+        $trending_images = collect($trending_images["images"]);
+
+        $processed_images = self::_processContent($trending_images);
+        return $processed_images;
+    }
+
+    private static function _processContent(&$trending_content)
+    {
+        return $trending_content->transform(function ($note) {
             if (isset($note["author"])) {
                 $note["author"]["content"] = json_decode($note["author"]["content"], true);
             }
             return $note;
         });
     }
+
+    
 }
